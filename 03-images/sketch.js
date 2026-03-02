48d5ec1b7606a8d813374a87b87a639e0e0c2683
wheat44 <wheatharvestertom@gmail.com>
2026-03-01 23:59:32 -0600

    changed images demo

--- 03-images/sketch.js
+++ 03-images/sketch.js
@@ -2,7 +2,6 @@
 
 function preload() {
   nalinda = loadImage('nalinda.jpg');
-  diddy = loadImage('diddy.jpg');
 }
 
 function setup() {
@@ -12,5 +11,4 @@ function setup() {
 function draw() {
   background(220);
   image(nalinda, mouseX, mouseY, windowWidth /8, windowHeight/8);
-  image(diddy,mouseX - 600 ,mouseY, diddy.height / 2, diddy.width/ 2);
 }
