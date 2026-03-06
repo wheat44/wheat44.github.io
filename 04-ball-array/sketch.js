bc33b23df6efb0207d4b1488b46e5b97c8304258
wheat44 <wheatharvestertom@gmail.com>
2026-03-06 10:11:47 -0600

    ball array demo

--- 04-ball-array/sketch.js
+++ 04-ball-array/sketch.js
@@ -11,28 +11,52 @@ function setup() {
 
 function draw() {
   background(12,124,91);
-  drawBall();
+  for (let ball of ballArray){
+    fill(ball.r,ball.g,ball.b);
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+    circle(ball.x,ball.y,ball.radius*2);
+    if (ball.x > width){
+      ball.x = 0;
+    }
+    if (ball.x < 0){
+      ball.x = width;
+    }
+    if (ball.y >height){
+      ball.y = 0;
+    }
+    if (ball.y < 0){
+      ball.y = height;
+    }
+  }
+
 }
 
 function mousePressed(){
-  spawnBall();
+  spawnBall(mouseX,mouseY);
 }
-function spawnBall(){
-  let someBall = {
-    x:random(width),
-    y:random(height),
+function spawnBall(_x, _y){
+  let ball = {
+    x:_x,
+    y:_y,
     dx: random (-5,5),
     dy: random(-5,5),
-    radius: random (10,30)
+    radius: random (10,30),
+    r: random(0,255),
+    g:random(0,255),
+    b: random(0,255),
   };
-  ballArray.push(someBall);
+
+  ballArray.push(ball);
 }
 
 function drawBall(){
   for (let ball of ballArray){
-    ball.x += ball.dx
-    ball.y += ball.dy
-    circle(ball.x,ball.y,ball.radius*2)
-
   }
+}
+
+
+function windowResized(){
+  windowWidth = windowWidth;
+  windowHeight = windowHeight;
 }
