0e32183fce698bc849aadd105da04b0bbde95bc3
wheat44 <wheatharvestertom@gmail.com>
2026-03-18 01:12:33 -0600

    Version - handin. Backgrounds and audio added, full functionality implimented.  Ready for handin.

--- array-assignment/sketch.js
+++ array-assignment/sketch.js
@@ -1,17 +1,32 @@
-// Array Assignmemnt - Ride the buss
+// Array Assignmemnt - Ride the bus
 // Tj Ham
-// Date
-//
+// 3/17/2026
+
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+//my project serves as a clean, streamlined example of what my previous card game should have been more like. it includes features such as:
+//-no duplicate card drawing
+//-sound effects
+//-polished visuals
+//-advanced array and object notation use (even both at the same time)
+//-clean draw loop
+//-fully implimented window resize features (all features designed with this in mind)
+//-advanced logic for gameplay mechanics
+//-fonts
+
+//The game uses some assets from my previous project, but otherwise it is fundimentally very different using cleaner solutions for cards, completely different logic systems
+//and a complicated loop/array based button system that im very proud of. its seriously the most beatiful thing ive ever created in my life. Hope you enjoy!
 
 
+///define gamestate
 let state = 'menu';
 
+///define variables for images, sounds, fonts, card values and suits, buttons and options
 let suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
 let values = [ "ace","2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
 let cardImages = [];
-let key;
+let options = [];
+let buttons = [];
+///define a array of objects to hold the value, suit and key for each card
 let cards = [
   {value: "", suit: "",key:""},
   {value: "", suit: "",key:""},
@@ -20,7 +35,7 @@ let cards = [
 ];
 
 
-
+///define locations and dimensions for buttons and cards
 let buttonX;
 let buttonY;
 let buttonH;
@@ -29,18 +44,23 @@ let cardWidth;
 let cardHeight;
 let cardX;
 let cardY;
+let spacing;
+let totalWidth;
 
 
+///playstage tracks what card is show and what options can be selected.
 let playStage = 0;
 
-let bet = 0;
+let bet = 100;
 let playerMoney = 5000;
+
+
 let choice;
 
 
 function preload() {
-  ///load background and meny images
-  main = loadImage("Assets/BG/mainBG.jpg");
+  ///load background and menu images
+  main = loadImage("Assets/BG/mainBG.png");
   menu = loadImage("Assets/BG/menu.png");
   instructions = loadImage('Assets/BG/instructions.png');
 
@@ -49,6 +69,13 @@ function preload() {
 
   //back of card
   bOC = loadImage('Assets/Cards/back_of_card.png');
+
+  /// load sound effects
+  win = loadSound('Assets/SFX/win.mp3');
+  lose = loadSound('Assets/SFX/lose.mp3');
+  cardFlip = loadSound('Assets/SFX/cardFlip.mp3');
+  betSound = loadSound('Assets/SFX/bet.mp3');
+  deal = loadSound('Assets/SFX/deal.mp3');
 
   ///load cards using a nested loop
   for (let index = 0; index < 4; index++) {
@@ -63,28 +90,31 @@ function preload() {
 }
 
 function setup() {
+  ///function setup, define canvas and variables for button and card dimensions and locations.
   createCanvas(windowWidth, windowHeight);
-  buttonH = windowHeight/ 15;
-  buttonW = windowWidth / 15;
+  buttonH = windowHeight/ 10;
+  buttonW = windowWidth / 10;
   buttonX = windowWidth / 2;
-  buttonY = windowHeight / 1.5;
-  cardHeight = windowHeight/8;
-  cardWidth = windowWidth/16;
-  cardX = width / 3;
-  cardY = height / 2.5;
+  buttonY = windowHeight / 1.2;
+  cardHeight = windowHeight/6;
+  cardWidth = windowWidth/13;
+  spacing = width / 10;
+  totalWidth = (3 * spacing + cardWidth); // distance from first to last card
+  cardX = width / 2 - totalWidth / 2;
+  cardY = height / 2.2;
 }
 
 function draw() {
+  ///draw loop
   displayBG();
   displayButtons();
   displayBet();
   displayCardValue();
-  decideCardValue();
-  calcResult();
-  console.log(state);
+
 }
 
 function displayBG(){
+  ///display background based on game state
   imageMode(CORNER);
   if (state === 'menu'){
     image(menu,0,0, windowWidth, windowHeight);
@@ -98,182 +128,292 @@ function displayBG(){
 }
 
 function mouseClicked(){
+  ///mouseclicked handles all mouse interactions
+  /// check if mouse is within boundaries for menu and instructions buttons
   if (state === 'menu') {
-    if (mouseX > (buttonX - 1/2 * buttonW) && mouseX < (buttonX + 1/2 * buttonW) && mouseY > (buttonY - 1/2 *buttonH) && mouseY < (buttonY + 1/2 *buttonH )){
+    if (mouseX > buttonX - buttonW/2 &&mouseX < buttonX + buttonW/2 &&mouseY > buttonY - buttonH/2 &&mouseY < buttonY + buttonH/2
+    ){
+      ///change state and play sound 
+      cardFlip.play();
       state = 'instructions';
     }
   }
+
   else if (state === 'instructions') {
-    if (mouseX > (buttonX - 1/2 * buttonW) && mouseX < (buttonX + 1/2 * buttonW) && mouseY > (buttonY - 1/2 *buttonH) && mouseY < (buttonY + 1/2 *buttonH )){
-      state = 'main'; 
+    if (mouseX > buttonX - buttonW/2 && mouseX < buttonX + buttonW/2 && mouseY > buttonY - buttonH/2 && mouseY < buttonY + buttonH/2){
+      cardFlip.play();
+      state = 'main';
     }
   }
-  else if (state === 'main' || state === 'redo') {
-    if (mouseX > (buttonX - 1/2 * buttonW) && mouseX < (buttonX + 1/2 * buttonW) && mouseY > (buttonY - 1/2 *buttonH) && mouseY < (buttonY + 1/2 *buttonH )){
-      state = 'play'; 
+
+  else if (state === 'main' || state === 'redo' || playStage === 4) {
+    ///deal button
+    if (mouseX > buttonX - buttonW/2 && mouseX < buttonX + buttonW/2 && mouseY > buttonY - buttonH/2 && mouseY < buttonY + buttonH/2){
+      deal.play();
+      state = 'play';
+      playStage = 0;
+      choice = "";
+      decideCardValue();
+      /// change bet
+      playerMoney -= bet;
     }
   }
-  ///red pressed
-  if (state === 'play') {
-    if (mouseX > (buttonX - 1/2 * buttonW - (width/20)) && mouseX < (buttonX + 1/2 * buttonW - (width/20)) && mouseY > (buttonY - 1/2 *buttonH) && mouseY < (buttonY + 1/2 *buttonH )){
-      choice = 'red';
-      console.log("button worked red");
-      if (cards[playStage].suit === 'Hearts' || cards[playStage].suit === 'Diamonds'){
-        playStage ++;
-      }  
-      else{
-        state = 'redo';
-        console.log("wrong");
-      }  
+
+  else if (state === 'play') {
+    ///for loop to check if mouse is within boundaries of any of the option buttons and determine which button was clicked based on the label of the button object.
+    for (let i = 0; i < buttons.length; i++){
+      let b = buttons[i];
+
+      if (mouseX > b.x - b.w/2 && mouseX < b.x + b.w/2 && mouseY > b.y - b.h/2 && mouseY < b.y + b.h/2){
+        choice = b.label;
+        calcResult();
+        cardFlip.play();
+        break;
+      }
     }
-  }
-  ///black pressed
-  if (state === 'play') {
-    if (mouseX > (buttonX - 1/2 * buttonW + (width/20)) && mouseX < (buttonX + 1/2 * buttonW + (width/20)) && mouseY > (buttonY - 1/2 *buttonH) && mouseY < (buttonY + 1/2 *buttonH )){
-      choice = 'black';
-      console.log("button worked black");
-      if (cards[playStage].suit === 'Spades' || cards[playStage].suit === 'Clubs'){
-        playStage ++;
-      }  
-      else{
-        state = 'redo';
-        console.log("wrong");
-      }  
+      ///check if cash out button is clicked
+    if (mouseX > windowWidth * 0.9 - buttonW/2 && mouseX < windowWidth * 0.9 + buttonW/2 && mouseY > windowHeight * 0.9 - buttonH/2 && mouseY < windowHeight * 0.9 + buttonH/2 && playStage >=1){
+      state = 'main';
+      ///change player money based on bet and playstage
+      playerMoney += bet + bet * (playStage + 1);
     }
   }
 }
+  
 
-function keyPressed(){
-  /// r to reset and space to deal cards
-  if (key === "r" ) {
-    state = "menu";
-  }
-  if (keyCode === 32 && state === 'main'){
-    state = 'play';
 
-  }
-  if (keyCode === 38 && state === 'play' && playStage < 4){
-    playStage ++;
-
-  }
-}
 
 function displayButtons(){
+  textFont('Courier New');
+  textSize(20);
+  rectMode(CENTER);
+  textAlign(CENTER, CENTER);
+
+  ///reset buttons and options arrays
+  buttons = [];
+  options = [];
+
+  ///Start button during menu state
   if (state === 'menu'){
-    textFont('Courier New');
-    textSize(20);
-    fill(0,222,41);
-    rectMode(CENTER);
-    textAlign(CENTER);
+    fill(212,141,51);
     rect(buttonX, buttonY, buttonW, buttonH);
     fill('black');
-    text('START!',buttonX, buttonY);
-  }
-  else if (state === 'instructions'){
-    textFont('Courier New');
-    textSize(20);
-    fill(0,222,41);
-    rectMode(CENTER);
-    textAlign(CENTER);
+    text('START!', buttonX, buttonY);
+  } 
+
+  ///okay button during instructions state
+  else if (state === 'instructions'){ 
+    fill(212,141,51);
     rect(buttonX, buttonY, buttonW, buttonH);
     fill('black');
-    text('OKAY!',buttonX, buttonY);
-  }
-  else if (state === 'main'){
-    textFont('Courier New');
-    textSize(20);
-    fill(0,222,41);
-    rectMode(CENTER);
-    textAlign(CENTER);
-    rect(buttonX, buttonY, buttonW, buttonH);
-    fill('black');
-    text('Deal!',buttonX, buttonY);
+    text('OKAY!', buttonX, buttonY);
   }
 
-  if (state === 'play'){
-    textFont('Courier New');
-    textSize(20);
-    fill("red");
-    rectMode(CENTER);
-    textAlign(CENTER);
-    rect(buttonX - (width/20), buttonY, buttonW, buttonH);
-    fill('white');
-    text('red',buttonX - (width/20), buttonY);
+  /// deal button during main and redo states
+  else if (state === 'main' || state === 'redo' || playStage === 4){
+    fill(212,141,51);
+    rect(buttonX, buttonY, buttonW, buttonH);
+    fill('black');
+    text('Deal!', buttonX, buttonY);
   }
-  if (state === 'play'){
-    textFont('Courier New');
-    textSize(20);
-    fill("black");
+
+  ///buttons for each stage of the game during play state
+  else if (state === 'play'){
+    ///cash out button
     rectMode(CENTER);
-    textAlign(CENTER);
-    rect(buttonX + (width/20), buttonY, buttonW, buttonH);
+    fill(79);
+    rect(windowWidth * 0.9, windowHeight * 0.9, buttonW, buttonH);
     fill('white');
-    text('Black',buttonX + (width/20), buttonY);
+    text('Cash Out', windowWidth * 0.9, windowHeight * 0.9);
+
+
+    if (playStage === 0){
+      options = ['Red', 'Black'];
+    }
+    else if (playStage === 1){
+      options = ['Higher', 'Lower', 'Same'];
+    }
+    else if (playStage === 2){
+      options = ['Inside', 'Outside', 'Same'];
+    }
+    else if (playStage === 3){
+      options = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
+    }
+
+    ///create buttons equally spaced based on number of options and display them using button object.
+    for (let i = 0; i < options.length; i++){
+      let x = buttonX + (i - (options.length - 1) / 2) * (buttonW + 10);
+
+      ///define the buttons that have to be drawn
+      buttons.push({
+        label: options[i],
+        x: x,
+        y: buttonY,
+        w: buttonW,
+        h: buttonH
+      });
+
+      ///choose colours for red and black buttons and gray for the rest
+      if (options[i] === 'Red' || options[i] === 'Hearts' || options[i] === 'Diamonds'){
+        fill('red');
+      }
+      else if (options[i] === 'Black' || options[i] === 'Spades' || options[i] === 'Clubs'){
+        fill('black');
+      }
+      else {
+        fill(79);
+      }
+
+      ///display the buttons
+      rect(x, buttonY, buttonW, buttonH);
+      fill('white');
+      text(options[i], x, buttonY);
+    }
   }
 }
+
+function calcResult(){
+  ///function calculates result after button push
+  if (playStage === 0){
+    if ((choice === 'Red' && (cards[0].suit === 'Hearts' || cards[0].suit === 'Diamonds')) ||(choice === 'Black' && (cards[0].suit === 'Spades' || cards[0].suit === 'Clubs'))){
+      playStage++;
+    } 
+    else {
+      state = 'redo';
+      playStage++;
+      lose.play();
+    }
+  }
+
+  else if (playStage === 1){
+    ///assing curr variable to track higher/lower variables 
+    let curr = values.indexOf(cards[1].value); 
+    let prev = values.indexOf(cards[0].value);
+
+    if ((choice === 'Higher' && curr > prev) ||(choice === 'Lower' && curr < prev) ||(choice === 'Same' && curr === prev)){
+      playStage++;
+    } 
+    else {
+      state = 'redo';
+      playStage++;
+      lose.play();
+    }
+
+  }
+
+
+  else if (playStage === 2){
+    ///create temp variables of last 2 cards and current card to compare if card is inside our outside previous
+    let a = values.indexOf(cards[0].value);
+    let b = values.indexOf(cards[1].value);
+    let c = values.indexOf(cards[2].value);
+
+    ///create min and maxval variable to choose between which is bigger and smaller to decide what peramiters choice must bein
+    let minVal = Math.min(a, b);
+    let maxVal = Math.max(a, b);
+
+    ///compare choice to peramiters
+    if ((choice === 'Inside' && c > minVal && c < maxVal) || (choice === 'Outside' && (c < minVal || c > maxVal)) || (choice === 'Same' && (c === a || c === b))){
+      playStage++;
+    } 
+
+    else {
+      state = 'redo';
+      playStage++;
+      lose.play();
+    }
+  }
+
+  ///check final stage if player chose correct suit
+  else if (playStage === 3){
+    if (cards[3].suit === choice){
+      playStage++;
+      win.play();
+      ///change player money based on bet and playstage
+      playerMoney += bet + bet * (playStage + 1);
+    } 
+    else {
+      state = 'redo';
+      playStage++;
+      lose.play();
+    }
+  }
+}
+
 
 function mouseWheel(event) {
   ///scrolling down
-  if (event.delta > 0 && bet > 0 && state === 'main' && bet <= playerMoney){
+  if (event.delta > 0 && bet > 0 && (state === 'main' ||  state === 'redo') && bet <= playerMoney){
     bet -= 25;
+    betSound.play();
   }
   ///scrolling up
-  else if(event.delta < 0 && bet < 1000  && state === 'main' && bet <= playerMoney){
+  else if(event.delta < 0 && bet < 1000  && (state === 'main' ||  state === 'redo') && bet + 25 <= playerMoney){
     bet += 25;
+    betSound.play();
   }
   ///prevent screen from scrolling when mouse scrolls 
   return false;
 }
 
 function displayBet(){
-  if (state === 'main'){
+  ///display the player bet and money amount
+  if (state === 'main' || state === 'play' || state === 'redo'){
     textFont(font);
-    fill(194,146,78);
-    textSize(30);
+    fill(212,141,51);
+    textSize(40);
     let amount = bet;
-    text("Bet: $" + amount, windowWidth * 0.1, windowHeight * 0.2);
+    text("Bet: $" + amount, windowWidth * 0.1, windowHeight * 0.88);
     let money = playerMoney;
-    text("Money: $" + money, windowWidth * 0.1, windowHeight * 0.25);
-    ellipseMode(CENTER);
-    fill(196, 180,150);
-    ellipse(windowWidth * 0.15, windowWidth* 0.23, 80, 40, 6);
-    
+    text("Money: $" + money, windowWidth * 0.1, windowHeight * 0.92); 
+  }
+
+  ///check if bet is invalid
+  if (bet >playerMoney){
+    bet = playerMoney;
+  }
+
+  ///check if playerMoney is greater than 0 
+  if (playerMoney <0){
+    bet = 0
+    playerMoney = 0
   }
 }
 
 function displayCardValue(){
-  if (state === 'play'){
+  ///function initially displays blank back of cards untill correct playstage, which then reads key and displays images
+  if (state === 'play' || state === 'redo'){
     for (let i = 0; i < 4; i++){
-      if (state === 'play'){
+      if (state === 'play' || state === 'redo'){
         if (playStage < 1){
-          image(bOC, cardX + i * (width/10), cardY, cardWidth, cardHeight);
+          image(bOC, cardX + i * spacing, cardY, cardWidth, cardHeight);
         }
         else if (playStage === 1){
           if (i >= 1){
-            image(bOC, cardX + i * (width/10), cardY, cardWidth, cardHeight);
+            image(bOC, cardX + i * spacing, cardY, cardWidth, cardHeight);
           }
           else {
-            image(cardImages[cards[i].key], cardX + i * (width/10), cardY, cardWidth, cardHeight);
+            image(cardImages[cards[i].key], cardX + i * spacing, cardY, cardWidth, cardHeight);
           }
         }
         else if (playStage === 2){
           if (i >= 2){
-            image(bOC, cardX + i * (width/10), cardY, cardWidth, cardHeight);
+            image(bOC, cardX + i * spacing, cardY, cardWidth, cardHeight);
           }
           else {
-            image(cardImages[cards[i].key], cardX + i * (width/10), cardY, cardWidth, cardHeight);
+            image(cardImages[cards[i].key], cardX + i * spacing, cardY, cardWidth, cardHeight);
           }
         }
         else if (playStage === 3){
           if (i >= 3){
-            image(bOC, cardX + i * (width/10), cardY, cardWidth, cardHeight);
+            image(bOC, cardX + i * spacing, cardY, cardWidth, cardHeight);
           }
           else {
-            image(cardImages[cards[i].key], cardX + i * (width/10), cardY, cardWidth, cardHeight);
+            image(cardImages[cards[i].key], cardX + i * spacing, cardY, cardWidth, cardHeight);
           }
         }
         else if (playStage === 4){
-          image(cardImages[cards[i].key], cardX + i * (width/10), cardY, cardWidth, cardHeight);
+          image(cardImages[cards[i].key], cardX + i * spacing, cardY, cardWidth, cardHeight);
         }
       }
     }
@@ -281,46 +421,63 @@ function displayCardValue(){
 }
 
 function decideCardValue(){
-  if (state !== 'play'){
+  if (state === 'play' || state === 'redo'){
     for (let i = 0; i < 4; i++){
 
-      let valueIndex = floor(random(0, 13));
-      let suitIndex = floor(random(0, 4));
-        
-
-      cards[i].value = values[valueIndex];
-      cards[i].suit = suits[suitIndex];
-      key = cards[i].value + '_' + cards[i].suit;
-      cards[i].key = key;
+      ///assigne duplicate variable
+      let duplicate = true;
       
+      while (duplicate){
+        duplicate = false;
+        let valueIndex = floor(random(0, 13));
+        let suitIndex = floor(random(0, 4));
+
+        ///assign temporary variables to check against during for loop
+        let tempValue = values[valueIndex];
+        let tempSuit = suits[suitIndex];
+        let tempKey = tempValue + '_' + tempSuit;
+
+        ///check against chosen card\
+        for (let f = 0; f < i; f++){
+          if (tempKey === cards[f].key){
+            duplicate = true;
+          }
+        }
+
+        ///only assigns permanent variables after confirming not duplicate
+        if (duplicate === false){
+          cards[i].value = tempValue;
+          cards[i].suit = tempSuit;
+          cards[i].key = tempKey;
+        }
+      }   
     }
-  } 
-}
-
-function playButtons(){
-
-
-}
-
-function calcResult(){
-  if (playStage === 4){
-    state = 'redo';
-    playStage = 0;
-
   }
-
 }
 
+
+function keyPressed(){
+  /// r to reset 
+  if (key === "r") {
+    state = "menu";
+    playStage = 0;
+    choice = "";
+  }
+}
+    
 
 
 function windowResized() {
+  /// redefine variables when window is resized.
   resizeCanvas(windowWidth, windowHeight);
-  buttonH = windowHeight/ 15;
-  buttonW = windowWidth / 15;
+  buttonH = windowHeight/ 10;
+  buttonW = windowWidth / 10;
   buttonX = windowWidth /2;
-  buttonY = windowHeight / 1.5;
-  cardWidth = windowWidth / 8;
-  cardHeight = windowHeight/16;
-  cardX = windowWidth / 4 ;
-  cardY = windowHeight / 2.5;
+  buttonY = windowHeight / 1.2;
+  cardWidth = windowWidth / 14;
+  cardHeight = windowHeight/ 6 ;
+  spacing = width / 10;
+  totalWidth = (3 * spacing + cardWidth); // distance from first to last card
+  cardX = width / 2 - totalWidth / 2;
+  cardY = height / 2.2;
 }
